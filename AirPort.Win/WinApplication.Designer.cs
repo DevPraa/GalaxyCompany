d57d848393ea8197256c943e5924f336cb1979fa
Viktor Tolkachev <tip67903@gmail.com>
2019-08-03 20:17:24 +0300

    Добавьте файлы проекта.

---
+++ AirPort.Win/WinApplication.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace AirPort.Win {
+    partial class AirPortWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new AirPort.Module.AirPortModule();
+            this.module4 = new AirPort.Module.Win.AirPortWindowsFormsModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
+            this.reportsWindowsFormsModuleV2 = new DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.validationWindowsFormsModule = new DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            //
+            // reportsModuleV2
+            //
+            this.reportsModuleV2.EnableInplaceReports = true;
+            this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+            this.reportsModuleV2.ShowAdditionalNavigation = false;
+            this.reportsModuleV2.ReportStoreMode = DevExpress.ExpressApp.ReportsV2.ReportStoreModes.XML;
+            //
+            // validationModule
+            //
+            this.validationModule.AllowValidationDetailsAccess = true;
+            this.validationModule.IgnoreWarningAndInformationRules = true;
+            // 
+            // AirPortWindowsFormsApplication
+            // 
+            this.ApplicationName = "AirPort";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.reportsModuleV2);
+            this.Modules.Add(this.reportsWindowsFormsModuleV2);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.validationWindowsFormsModule);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.AirPortWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.AirPortWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private AirPort.Module.AirPortModule module3;
+        private AirPort.Module.Win.AirPortWindowsFormsModule module4;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.ReportsV2.ReportsModuleV2 reportsModuleV2;
+        private DevExpress.ExpressApp.ReportsV2.Win.ReportsWindowsFormsModuleV2 reportsWindowsFormsModuleV2;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.Validation.Win.ValidationWindowsFormsModule validationWindowsFormsModule;
+    }
+}
