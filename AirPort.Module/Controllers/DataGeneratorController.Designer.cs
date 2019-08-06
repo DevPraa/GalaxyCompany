cb66aaf2fee4bde37e7b08806f71223b98cee3a8
Viktor Tolkachev <tip67903@gmail.com>
2019-08-06 06:56:32 +0300

    Added DataGeneratorController and Action

---
+++ AirPort.Module/Controllers/DataGeneratorController.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace AirPort.Module.Controllers
+{
+    partial class DataGeneratorController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DGC_Generate_Action = new DevExpress.ExpressApp.Actions.SimpleAction();
+            // 
+            // DGC_Generate_Action
+            // 
+            this.DGC_Generate_Action.Caption = "DGC_Generate_Action_Id";
+            this.DGC_Generate_Action.ConfirmationMessage = null;
+            this.DGC_Generate_Action.Id = "DGC_Generate_Action_Id";
+            this.DGC_Generate_Action.ToolTip = null;
+            this.DGC_Generate_Action.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.DGC_Generate_Action_Execute);
+            // 
+            // DataGeneratorController
+            // 
+            this.Actions.Add(this.DGC_Generate_Action);
+            this.TargetWindowType = DevExpress.ExpressApp.WindowType.Main;
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction DGC_Generate_Action;
+    }
+}
---
+++ AirPort.Module/Controllers/DataGeneratorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Validation;
+
+namespace AirPort.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppWindowControllertopic.aspx.
+    public partial class DataGeneratorController : WindowController
+    {
+        public DataGeneratorController()
+        {
+            InitializeComponent();
+            // Target required Windows (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target Window.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void DGC_Generate_Action_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            
+        }
+    }
+}
