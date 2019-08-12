8e79bee463d2c44cb182d9b61ba5d4d8a079e1a0
Viktor Tolkachev <tip67903@gmail.com>
2019-08-12 04:10:48 +0300

    Added showing Report for ListView Pilot

---
+++ AirPort.Module/Controllers/PilotViewController.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace AirPort.Module.Controllers
+{
+    partial class PilotViewController
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ShowReportPilot_Action = new DevExpress.ExpressApp.Actions.SimpleAction();
+            // 
+            // ShowReportPilot_Action
+            // 
+            this.ShowReportPilot_Action.Caption = "ShowReportPilot_Action";
+            this.ShowReportPilot_Action.ConfirmationMessage = null;
+            this.ShowReportPilot_Action.Id = "ShowReportPilot_Action";
+            this.ShowReportPilot_Action.ImageName = "BO_Report";
+            this.ShowReportPilot_Action.TargetObjectType = typeof(AirPort.Module.BusinessObjects.Galaxy_db.rb_Pilot);
+            this.ShowReportPilot_Action.TargetViewType = DevExpress.ExpressApp.ViewType.ListView;
+            this.ShowReportPilot_Action.ToolTip = null;
+            this.ShowReportPilot_Action.TypeOfView = typeof(DevExpress.ExpressApp.ListView);
+            this.ShowReportPilot_Action.Execute += new DevExpress.ExpressApp.Actions.SimpleActionExecuteEventHandler(this.ShowReportPilot_Action_Execute);
+            // 
+            // PilotViewController
+            // 
+            this.Actions.Add(this.ShowReportPilot_Action);
+            this.Activated += new System.EventHandler(this.PilotViewController_Activated);
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.Actions.SimpleAction ShowReportPilot_Action;
+    }
+}
---
+++ AirPort.Module/Controllers/PilotViewController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.ReportsV2;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace AirPort.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class PilotViewController : ViewController
+    {
+        public PilotViewController()
+        {
+            var t = View;
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+
+        private void ShowReportPilot_Action_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+             if (controller != null)
+            {
+                controller.ShowPreview(handle);
+            }
+        }
+
+        string handle;
+        ReportServiceController controller;
+        private void PilotViewController_Activated(object sender, EventArgs e)
+        {
+            IObjectSpace objectSpace =
+    ReportDataProvider.ReportObjectSpaceProvider.CreateObjectSpace(typeof(ReportDataV2));
+            IReportDataV2 reportData =
+                objectSpace.FindObject<ReportDataV2>(
+                CriteriaOperator.Parse("[DisplayName] = 'Pilot Report'"));
+            handle = ReportDataProvider.ReportsStorage.GetReportContainerHandle(reportData);
+            controller = Frame.GetController<ReportServiceController>();
+        }
+    }
+}
